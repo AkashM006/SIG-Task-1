687546f4ed3dcd19bc035b10df85f57089c89430
Akash <69391881+AkashM006@users.noreply.github.com>
2021-07-18 19:21:45 +0530

    Update index.js
    Removed all logs

--- index.js
+++ index.js
@@ -124,7 +124,6 @@ avoidRefresh.forEach(aButton => {
      })
 });
 
-console.log(saveButton);
 saveButton.addEventListener("click", () => {
      let text = textContent.value;
      setLocalData("content", text);
