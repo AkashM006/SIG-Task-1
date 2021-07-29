d5d6be32d64e986ddc410a07d2c58f7f45138c2c
AkashM006 <akashm006@outlook.com>
2021-07-29 17:54:26 +0530

    Added minified files for js scripts

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const deleteButton = document.querySelector(".delete-btn");
 const avoidRefresh = document.querySelectorAll("a");
 const toggleTheme = document.querySelector(".toggle");
 const toggleSub = document.querySelector(".opt-toggle");
-const icons = document.querySelector("#icon");
+
 const localData = JSON.parse(window.localStorage.getItem("data")) ?? {};
 const iTheme = document.querySelector(".icon-dp");
 const modal = document.querySelector(".modal__wrapper");
@@ -21,13 +21,17 @@ let lastContent = "";
 const toggler = () => {
      let data = JSON.parse(window.localStorage.getItem("data")) ?? { dark: false };
      setLocalData("dark", !data.dark ?? true);
-}
+};
 
 const setLocalData = (key, value) => {
      let data = JSON.parse(window.localStorage.getItem("data")) ?? { content: "Write something and save it in your local storage..!", dark: false };
-     if (key === "content")
-          lastContent = data["content"];
      data[key] = value;
+     if (key === "content") {
+          lastContent = data["content"];
+          data['content'] = value;
+          if (value.trim().length === 0)
+               data['content'] = "Write something and save it in your local storage..!";
+     }
      window.localStorage.setItem("data", JSON.stringify(data));
      if (key === "dark") {
           if (value === true)
@@ -35,23 +39,24 @@ const setLocalData = (key, value) => {
           else
                setLightMode();
      }
-}
+     setTextFieldData();
+};
 
 const deleteLocalData = (key) => {
      let data = JSON.parse(window.localStorage.getItem("data"));
      if (key === "content")
           lastContent = data["content"];
      data[key] = 'Write something and save it in your local storage..!';
-     window.localStorage.setItem("data", JSON.stringify(data))
-}
+     window.localStorage.setItem("data", JSON.stringify(data));
+};
 
 const setTextFieldData = () => {
-     let data = JSON.parse(window.localStorage.getItem("data")) ?? { content: "Write something and save it in your local storage..!", dark: false };
+     let data = JSON.parse(window.localStorage.getItem("data")) ?? { content: "Write something and save it in your local storage..!" };
      textContent.value = data.content ?? "Write something and save it in your local storage..!";
-}
+};
 
 const setModalContent = (text) => {
-     modalContent.innerHTML = text ?? "Successfully done"
+     modalContent.innerHTML = text ?? "Successfully done";
 };
 
 const setDarkMode = () => {
@@ -60,7 +65,7 @@ const setDarkMode = () => {
      iTheme.classList.remove("fa-moon-o");
      iTheme.classList.add("fa-sun-o");
      document.body.classList.add("dark");
-}
+};
 
 const setLightMode = () => {
      icon.classList.remove("fa-sun-o");
@@ -68,16 +73,10 @@ const setLightMode = () => {
      iTheme.classList.remove("fa-sun-o");
      iTheme.classList.add("fa-moon-o");
      document.body.classList.remove("dark");
-}
+};
 
 const showModal = (text) => {
-     setModalContent(text)
-     // overlay.style.display = "block"
-     // document.body.style.height = "100%";
-     // document.body.style.overflow = 'hidden';
-     // modal.style.display = "block";
-     // modal.style.opacity = "100";
-     // modal.style.display = "block";
+     setModalContent(text);
      if (i++ === 0) {
           modal.classList.add("modal-active");
           timer.classList.add("timer-active");
@@ -91,32 +90,25 @@ const showModal = (text) => {
                timer.classList.add("timer-active");
           }, 10);
      }
-     // modal.classList.toggle("modal-active");
-}
+};
 
 const closeModal = () => {
      modal.classList.remove("modal__inactive");
      setTimeout(() => {
           modal.classList.add("modal__inactive");
      }, 10);
-     // modal.classList.remove("modal-active");
-     // timer.classList.remove("timer-active");
-     // modal.style.display = "none"
-     // overlay.style.display = "none"
-     // document.body.style.height = "auto";
-     // document.body.style.overflow = 'auto';
-}
+};
 
 const undo = () => {
      textContent.value = lastContent;
      setLocalData("content", lastContent ?? "");
      lastContent = "";
-}
+};
 
 modalButton.addEventListener("click", (event) => {
      undo();
      closeModal();
-})
+});
 
 avoidRefresh.forEach(aButton => {
      aButton.addEventListener("click", (event) => {
@@ -125,20 +117,20 @@ avoidRefresh.forEach(aButton => {
 });
 
 saveButton.addEventListener("click", () => {
-     let text = textContent.value;
+     let text = String(textContent.value);
      setLocalData("content", text);
      showModal("Content saved to local storage!");
-})
+});
 
 deleteButton.addEventListener("click", () => {
      deleteLocalData("content");
      showModal("Content deleted from your local storage!");
      setTextFieldData();
-})
+});
 
 closeModalButton.addEventListener("click", () => {
      closeModal();
-})
+});
 
 toggleTheme.addEventListener("click", toggler);
 
